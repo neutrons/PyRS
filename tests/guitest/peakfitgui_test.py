754b47816a40811f4b78a4076d60ab3d4e24a2b1
Wenduo Zhou <zhouw@ornl.gov>
2018-04-27 16:50:04 -0400

    Refs #1. Continued to set up tests.

--- tests/guitest/peakfitgui_test.py
+++ tests/guitest/peakfitgui_test.py
@@ -1,11 +1,43 @@
+#!/usr/bin/python
 # In order to test the peak fit window (GUI)
+import sys
 import pyrs.interface
+from pyrs.interface import fitpeakswindow
 import pyrs.core
+from pyrs.core import pyrscore
+from PyQt4.QtGui import QApplication
 
 
 def test_main():
     """
     test main
     """
-    fit_window = pyrs.interface.fitpeakwindow(None)
-    pyrs_core = pyrs.core.pyrscore()
+    fit_window = fitpeakswindow.FitPeaksWindow(None)
+    # fit_window.show()
+    pyrs_core = pyrscore.PyRsCore()
+
+    fit_window.ui.lineEdit_expFileName.setText('tests/testdata/BD_Data_Log.hdf5')
+
+    return fit_window
+
+
+def main(argv):
+    """
+    """
+    if QApplication.instance():
+        _app = QApplication.instance()
+    else:
+        _app = QApplication(sys.argv)
+    return _app
+
+if __name__ == '__main__':
+    # Main application
+    print ('Test Peak Fit GUI')
+    app = main(sys.argv)
+
+    # this must be here!
+    test_window = test_main()
+    test_window.show()
+    # I cannot close it!  test_window.close()
+
+    app.exec_()
