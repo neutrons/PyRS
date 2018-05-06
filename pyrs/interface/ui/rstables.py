6246d3898a0592aac3f659e54173b2eb9ec642e6
Wenduo Zhou <zhouw@ornl.gov>
2018-05-06 13:39:21 -0400

    Refs #1. Added TableWidgets.

---
+++ pyrs/interface/ui/rstables.py
@@ -0,0 +1,15 @@
+# Module containing extended TableWidgets for PyRS project
+import NTableWidget
+
+
+class FitResultTable(NTableWidget.NTableWidget):
+    """
+
+    """
+    def __init__(self, parent):
+        """
+
+        """
+        super(FitResultTable).__init__(parent)
+
+        return
