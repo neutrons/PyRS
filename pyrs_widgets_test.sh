dbfeb75308d1bec41b5d5d87ae4c6c90912c745c
Wenduo Zhou <zhouw@ornl.gov>
2018-08-29 10:23:18 -0400

    Refs #15. Added new widget test script.

---
+++ pyrs_widgets_test.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+python setup.py pyuic
+python setup.py build
+if [ $1 ]; then
+    CMD=$1
+else
+    CMD=''
+fi
+MANTIDLOCALPATH=/home/wzz/Mantid_Project/builds/debug-master/bin/
+MANTIDMACPATH=/Users/wzz/MantidBuild/debug-stable/bin/
+MANTIDSNSDEBUGPATH=/SNS/users/wzz/Mantid_Project/builds/debug/bin/
+MANTIDPATH=$MANTIDMACPATH:$MANTIDLOCALPATH:$MANTIDSNSDEBUGPATH
+PYTHONPATH=$MANTIDPATH:$PYTHONPATH
+echo $PYTHONPATH
+
+PYTHONPATH=build/lib:$PYTHONPATH $CMD python tests/widgets/slice_view_widget_test.py
