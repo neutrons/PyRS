ab882e6f956a263518b12f3b96822a24dc833c34
Wenduo Zhou <zhouw@ornl.gov>
2018-10-12 14:57:33 -0400

    Refs #30. Fixed minor issues during demo.

--- pyrs/interface/ui/diffdataviews.py
+++ pyrs/interface/ui/diffdataviews.py
@@ -36,11 +36,11 @@ class Diffraction2DPlot(MplGraphicsPolarView):
 
         # project vector to XY plane, i.e., convert alpha (phi) azimuthal angle to r
         vec_r = np.sin(vec_alpha * np.pi / 180.)
-        vec_intensity = np.log(vec_intensity+1.)
+        vec_intensity = vec_intensity
 
         #     def plot_contour(self, vec_theta, vec_r, vec_values, max_r, r_resolution, theta_resolution):
         self._myCanvas.plot_contour(vec_theta=vec_beta, vec_r=vec_r, vec_values=vec_intensity, max_r=1.,
-                                    r_resolution=0.05, theta_resolution=3.)
+                                    r_resolution=0.05, theta_resolution=12.)
 
         self.show()
 
