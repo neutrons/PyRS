1aebc6b3d90b727f00425029decec46dc52cc9b4
Wenduo Zhou <zhouw@ornl.gov>
2019-09-03 11:14:30 -0400

    Starting.

--- pyrs_gui_test.sh
+++ pyrs_gui_test.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 python setup.py pyuic
 python setup.py build
-echo "Not used /home/wzz/Mantid_Project/builds/debug-master/bin/"
-MANTIDLOCALPATH=/home/wzz/Mantid_Project/debug/bin/
-MANTIDMACPATH=/Users/wzz/MantidBuild/debug/bin/
-# MANTIDSNSDEBUGPATH=/SNS/users/wzz/Mantid_Project/builds/debug/bin/
-MANTIDSNSDEBUGPATH=/opt/mantidnightly/bin/  # NIGHTLY for latest Pseudo-voigt
-MANTIDPATH=$MANTIDMACPATH:$MANTIDLOCALPATH:$MANTIDSNSDEBUGPATH
-PYTHONPATH=$MANTIDPATH:$PYTHONPATH
-echo "PYTHON PATH"
-echo $PYTHONPATH
-echo
 
 if [ $1 ]; then
     CMD=$1
@@ -18,6 +8,13 @@ else
     CMD=
     echo "1: peak fit, 2: texture, 3: strain stress, 4: manual reduction, 5: instrument geometry calibration"
 fi
+
+MANTIDLOCALPATH=/home/wzz/Mantid_Project/builds/mantid-python2/bin/
+MANTIDMACPATH=/Users/wzz/MantidBuild/debug/bin/
+MANTIDSNSDEBUGPATH=/opt/mantidnightly/bin/  # NIGHTLY for latest Pseudo-voigt
+MANTIDPATH=$MANTIDMACPATH:$MANTIDLOCALPATH:$MANTIDSNSDEBUGPATH
+PYTHONPATH=$MANTIDPATH:$PYTHONPATH
+PYRSPATH=build/lib.linux-x86_64-2.7/:build/lib/
 
 if [ "$1" = "1" ]
 then 
@@ -34,13 +31,13 @@ fi
 if [ "$1" = "3" ]
 then 
 	echo "Test strain/stress calculation module"
-	PYTHONPATH=build/lib:$PYTHONPATH build/scripts-2.7/strainstressgui_test.py
+	PYTHONPATH=$PYRSPATH:$PYTHONPATH build/scripts-2.7/strainstressgui_test.py
 fi
 
 if [ "$1" = "4" ]
 then 
 	echo "Test maual reduction mdoule"
-	PYTHONPATH=build/lib:$PYTHONPATH build/scripts-2.7/manualreduction_test.py
+	PYTHONPATH=$PYRSPATH:$PYTHONPATH build/scripts-2.7/manualreduction_test.py
 fi
 
 if [ "$1" = "5" ]
