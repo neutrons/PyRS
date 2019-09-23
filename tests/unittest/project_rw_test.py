b860e1e3f0d82f49a3e1aa80ec5ecd724a262a35
Wenduo Zhou <zhouw@ornl.gov>
2019-09-23 09:23:56 -0400

    Refs #93. Added the unit test for HiDRA project.

---
+++ tests/unittest/project_rw_test.py
@@ -0,0 +1,76 @@
+"""
+Test for reading and writing components to HiDRA project file
+"""
+
+def test_mask(remove_test_file):
+    """
+    Test methods to read and write mask file
+    Parameters
+    ----------
+    remove_test_file : bool
+        Flag to remove temporary testing file
+
+    Returns
+    -------
+    None
+    """
+    # Generate a HiDRA project file
+
+    # Create a detector mask
+
+    # Create a solid angle mask
+
+    # Write detector mask
+
+    # Write solid angle mask
+
+    # Close file
+
+    # Open file again
+
+    # Read detector mask & compare
+
+    # Read solid angle mask & compare
+
+    return
+
+
+def test_detector_efficiency(remove_test_file):
+    """
+    Test methods to read and write detector efficiency
+    Parameters
+    ----------
+    remove_test_file :  bool
+        Flag to remove temporary testing file
+
+    Returns
+    -------
+    None
+    """
+    # Generate a HiDRA project file
+
+    # Create a detector efficiency array
+
+    # Write to file
+
+    # Close file
+
+    # Open file again
+
+    # Read detector efficiency & compare
+
+    return
+
+
+def test_monochromator_setup(remove_test_file):
+    """
+
+    Parameters
+    ----------
+    remove_test_file
+
+    Returns
+    -------
+
+    """
+
