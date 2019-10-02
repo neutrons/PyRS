7dab62986314ef79267b0fa2e50fc982ff38577a
JeanBilheux <bilheuxjm@ornl.gov>
2019-10-02 17:03:56 -0400

    Started to replace qt import. This refs #99

---
+++ pyrs/utilities/__init__.py
@@ -0,0 +1,13 @@
+import os
+from qtpy.uic import loadUi
+
+
+def load_ui(ui_filename, baseinstance):
+    ui_filename = os.path.split(ui_filename[-1])
+    ui_path = os.path.dirname(ui.__file__)
+
+    # get the location of the ui directory
+    # this function assumes that all ui files are there
+    filename = os.path.join(ui_path, ui_filename)
+
+    return loadUi(filename, baseinstance=baseinstance)
