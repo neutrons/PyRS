f185d33c176f6e0b80055cc36445fdf7b1ccdf1c
Pete Peterson <petersonpf@ornl.gov>
2019-10-09 13:52:40 -0400

    Upgrade to versioneer v0.18

--- pyrs/__init__.py
+++ pyrs/__init__.py
@@ -1,7 +1,3 @@
-from pyrs. _version import get_versions
-__version__ = get_versions()['version']
-del get_versions
-
 __description__ = "Reduction software for HB2B"
 __url__ = 'https://github.com/neutrons/PyRS'
 
@@ -9,3 +5,7 @@ __author__ = 'W.Zhou'
 __email__ = 'zhouw@ornl.gov'
 
 __license__ = "The MIT License (MIT)"
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
