ec2894c5d6df22883d06c26bafa79ca6bd1a5643
fanchercm@ornl.gov <fanchercm@ornl.gov>
2019-11-14 09:56:11 -0500

    Updated calibration

--- pyrs/calibration/peakfit_calibration.py
+++ pyrs/calibration/peakfit_calibration.py
@@ -207,7 +207,10 @@ class PeakFitCalibration(object):
                                 tr_solver=tr_solver, max_nfev=max_nfev, args=args)
 
             J = out.jac
-            cov = np.linalg.inv(J.T.dot(J))
+            if np.max( J.shape ) > 1:
+                cov = np.linalg.inv(J.T.dot(J))
+            else:
+                cov = J.T.dot(J) 
 
             return [out.x, cov, out.status]
 
