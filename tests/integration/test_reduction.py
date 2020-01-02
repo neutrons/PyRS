d5eab3ad9255c13b087ab86f8a175a7e38aedda7
Wenduo Zhou <zhouw@ornl.gov>
2020-01-02 17:37:13 -0500

    Refs #237. Added a skippable performance test.

--- tests/integration/test_reduction.py
+++ tests/integration/test_reduction.py
@@ -1,7 +1,10 @@
+from pyrs.core.nexus_conversion import NeXusConvertingApp
 from pyrs.projectfile import HidraProjectFile
 from pyrs.utilities import calibration_file_io
 from pyrs.core import workspaces
 import pytest
+import time
+import os
 
 
 def test_calibration_json():
@@ -32,6 +35,33 @@ def test_calibration_json():
     # Reduce
     test_workspace = workspaces.HidraWorkspace('test calibration')
     test_workspace.load_hidra_project(project_file, load_raw_counts=True, load_reduced_diffraction=False)
+
+
+def test_performance_test():
+    """Performance test for converting from NeXus to sub runs
+
+    Returns
+    -------
+
+    """
+    # Set up
+    run_number = 1060
+    # Convert the NeXus to project
+    nexus_file = '/HFIR/HB2B/IPTS-22731/nexus/HB2B_{}.nxs.h5'.format(run_number)
+    if os.path.exists(nexus_file) is False:
+        pytest.skip('{} is not available for test performance.'.format(nexus_file))
+
+    project_file = 'HB2B_{}.h5'.format(run_number)
+
+    # Convert
+    time_0 = time.time()
+    converter = NeXusConvertingApp(nexus_file)
+    converter.convert()
+    time_1 = time.time()
+    converter.save(project_file, None)
+    time_2 = time.time()
+
+    print('Converting = {} seconds; Saving = {} seconds'.format(time_1 - time_0, time_2 - time_1))
 
 
 if __name__ == '__main__':
