f3992546df975e087dc93a4c41d2265aad2e95a3
Pete Peterson <petersonpf@ornl.gov>
2020-01-13 16:04:57 -0500

    Fixing broken tests

--- pyrs/dataobjects/sample_logs.py
+++ pyrs/dataobjects/sample_logs.py
@@ -9,6 +9,8 @@ __all__ = ['SampleLogs', 'SubRuns']
 def _coerce_to_ndarray(value):
     if isinstance(value, np.ndarray):
         return value
+    elif isinstance(value, SubRuns):
+        return value._value  # pylint: disable=protected-access
     else:
         return np.atleast_1d(value)
 
@@ -18,61 +20,84 @@ class SubRuns(Iterable):
     def __init__(self, subruns=None):
         '''Default is to create zero-length subruns. This is the only version of
         subrun that can have its value updated'''
-        self.__value = np.ndarray((0))
+        self._value = np.ndarray((0))
 
         if subruns is not None:
             self.set(subruns)
 
+    def __getitem__(self, key):
+        return self._value[key]
+
     def __eq__(self, other):
         other = _coerce_to_ndarray(other)
-        if other.size != self.__value.size:
+        if other.size != self._value.size:
             return False
         else:
-            return np.all(other == self.__value)
+            return np.all(other == self._value)
 
     def __ne__(self, other):
-        return not (self == other)
+        return not (self.__eq__(other))
 
     def __iter__(self):
-        yield self.__value.next()
+        iterable = self._value.tolist()
 
-    def __len__(self):
-        return self.size
+        return iterable.__iter__()
+
+    def __repr__(self):
+        return repr(self._value)
+
+    def __str__(self):
+        return str(self._value)
 
     @property
     def size(self):
-        return self.__value.size
+        return self._value.size
+
+    @property
+    def shape(self):
+        return self._value.shape
+
+    @property
+    def ndim(self):
+        return self._value.ndim
+
+    def __len__(self):
+        return self._value.size
 
     def empty(self):
-        return self.size == 0
+        return self._value.size == 0
 
     def set(self, value):
-        if not self.empty():
-            if self != value:
-                raise RuntimeError('Cannot change subruns when non-empty '
-                                   '(previous={}, new={})'.format(self.__value, value))
         value = _coerce_to_ndarray(value)
+        if not self.empty():
+            if self.__ne__(value):
+                raise RuntimeError('Cannot change subruns when non-empty '
+                                   '(previous={}, new={})'.format(self._value, value))
         if not np.all(value[:-1] < value[1:]):
             raise RuntimeError('subruns are not soryed in increasing order')
-        self.__value = value
+        self._value = value.astype(int)
+
+    def raw(self):
+        '''Raw copy of underlying values'''
+        return np.copy(self._value)
 
     def get_indices(self, subruns):
         '''Convert the list of subruns into indices into the subrun array'''
         if self.__eq__(subruns):
-            return np.arange(self.__value.size)
+            return np.arange(self._value.size)
         else:
             subruns = _coerce_to_ndarray(subruns)
             # look for the single value
             if subruns.size == 1:
-                indices = np.nonzero(self.__value == subruns[0])[0]
+                indices = np.nonzero(self._value == subruns[0])[0]
                 if indices.size > 0:
                     return indices
             # check that the first and last values are in the array
-            elif subruns[0] in self.__value and subruns[-1] in self.__value:
-                return np.searchsorted(self.__value, subruns)
+            elif subruns[0] in self._value and subruns[-1] in self._value:
+                return np.searchsorted(self._value, subruns)
 
         # fall-through is an error
-        raise IndexError('Failed to find subruns={}'.format(subruns))
+        raise IndexError('Failed to find subruns={} in {}'.format(subruns, self._value))
 
 
 class SampleLogs(MutableMapping):
