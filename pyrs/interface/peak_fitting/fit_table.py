e017f05f6330215d74452f152fdb74e1b702d1b4
JeanBilheux <bilheuxjm@ornl.gov>
2020-01-15 13:30:50 -0500

    fitting table working. this refs #233

--- pyrs/interface/peak_fitting/fit_table.py
+++ pyrs/interface/peak_fitting/fit_table.py
@@ -1,9 +1,9 @@
 import numpy as np
-
-from pyrs.core.peak_profile_utility import PeakShape
-
+from qtpy.QtWidgets import QTableWidgetItem
 
 class FitTable:
+
+    COL_INDEX_TO_ESCAPE = [1]
 
     def __init__(self, parent=None, fit_result=None):
         self.parent = parent
@@ -13,16 +13,24 @@ class FitTable:
         self._initialize_list_of_peaks()
         self._initialize_table()
 
-
     def populate_fit_result_table(self):
         _peak_selected = self.parent.ui.spinBox_peak_index.value()
         _peak_collection = self.fit_result.peakcollections[_peak_selected-1]  # peak 1 is at 0 index
 
         _value = self._get_value_to_display(_peak_collection)
 
-        import pprint
-        pprint.pprint("populate_fit_result_table")
-        pprint.pprint(_value.dtype)
+        for _row, _row_value in enumerate(_value):
+            self.parent.ui.tableView_fitSummary.insertRow(_row)
+            _global_col_index = 0
+
+            print(_row_value)
+
+            for _local_col_index, _col_value in enumerate(_row_value):
+                if _local_col_index in self.COL_INDEX_TO_ESCAPE:
+                    continue
+                _item = QTableWidgetItem(str(_col_value))
+                self.parent.ui.tableView_fitSummary.setItem(_row, _global_col_index, _item)
+                _global_col_index += 1
 
     def _get_value_to_display(self, peak_collection):
         if self.parent.ui.radioButton_fit_value.isChecked():
@@ -56,8 +64,21 @@ class FitTable:
             self.parent.ui.tableView_fitSummary.removeColumn(0)
 
     def _get_list_of_columns(self):
+        # we want to go from
+        # ('wsindex', 'peakindex', 'Mixing', 'Intensity', 'PeakCentre', 'FWHM', 'A0', 'A1', 'chi2')
+        # to
+        # ('Sub-run #', 'Mixing', ....)
+
         _peak_collection = self.fit_result.peakcollections[0]
         column_names = _peak_collection.parameters_values.dtype.names
-        return column_names
+        # remove second column
+        clean_column_names = []
+        for _col_index, _col_value in enumerate(column_names):
+            if _col_index in self.COL_INDEX_TO_ESCAPE:
+                continue
+            if _col_index == 0:
+                _col_value = 'Sub-run #'
+            clean_column_names.append(_col_value)
+        return clean_column_names
 
 
