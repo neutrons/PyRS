e2800e5e472b5304831787fc6e51845ccc23edba
Islam, Fahima F <islamff@ornl.gov>
2020-01-27 16:04:53 -0500

    Ref#303 flake correction and added extra testing parameters

--- tests/unit/test_peak_collection.py
+++ tests/unit/test_peak_collection.py
@@ -4,21 +4,22 @@ from pyrs.core.peak_profile_utility import PeakShape, BackgroundFunction, get_pa
 from pyrs.peaks import PeakCollection
 import pytest
 
-def check_peak_shape_enum(peak_shape):
+
+def check_peak_shape_enum(peak_shape, num_native_params):
     assert PeakShape.getShape(peak_shape) == PeakShape[peak_shape.upper()]
     assert PeakShape.getShape(peak_shape.upper()) == PeakShape[peak_shape.upper()]
     with pytest.raises(KeyError):
         PeakShape.getShape('non-existant-peak-shape')
 
-    assert len(PeakShape.getShape(peak_shape).native_parameters) == 3
+    assert len(PeakShape.getShape(peak_shape).native_parameters) == num_native_params
 
 
 def test_peak_shape_enum_gaussian():
-    check_peak_shape_enum('gaussian')
+    check_peak_shape_enum('gaussian', 3)
 
 
 def test_peak_shape_enum_pseudoVoigt():
-    check_peak_shape_enum('PseudoVoigt')
+    check_peak_shape_enum('PseudoVoigt', 4)
 
 
 def test_background_enum():
@@ -38,10 +39,12 @@ def check_peak_collection(peak_shape, NUM_SUBRUN, target_error):
     raw_peaks_array = np.zeros(NUM_SUBRUN, dtype=get_parameter_dtype(peak_shape, 'Linear'))
     if peak_shape == 'PseudoVoigt':
         raw_peaks_array['Intensity'] = [1, 2]
+        raw_peaks_array['FWHM'] = np.array([4, 5], dtype=float)
     else:
         raw_peaks_array['Height'] = [1, 2]
+        raw_peaks_array['Sigma'] = np.array([4, 5], dtype=float)
     raw_peaks_array['PeakCentre'] = [3, 4]
-    raw_peaks_array['Sigma'] = np.array([4, 5], dtype=float)
+
     # background terms are both zeros
     raw_peaks_errors = np.zeros(NUM_SUBRUN, dtype=get_parameter_dtype(peak_shape, 'Linear'))
 
@@ -65,26 +68,28 @@ def check_peak_collection(peak_shape, NUM_SUBRUN, target_error):
     np.testing.assert_equal(obs_eff_errors, target_error)
     np.testing.assert_equal(obs_eff_peaks['Center'], raw_peaks_array['PeakCentre'])
     if peak_shape == 'PseudoVoigt':
-        np.testing.assert_equal(obs_eff_peaks['Intensity'], raw_peaks_array['Height'])
+        np.testing.assert_equal(obs_eff_peaks['Intensity'], raw_peaks_array['Intensity'])
+        np.testing.assert_equal(obs_eff_peaks['FWHM'], raw_peaks_array['FWHM'])
     else:
         np.testing.assert_equal(obs_eff_peaks['Height'], raw_peaks_array['Height'])
-    np.testing.assert_equal(obs_eff_peaks['FWHM'], 2. * np.sqrt(2. * np.log(2.)) * raw_peaks_array['Sigma'])
+        np.testing.assert_equal(obs_eff_peaks['FWHM'], 2. * np.sqrt(2. * np.log(2.)) * raw_peaks_array['Sigma'])
     np.testing.assert_equal(obs_eff_peaks['A0'], NUM_SUBRUN * [0.])
     np.testing.assert_equal(obs_eff_peaks['A1'], NUM_SUBRUN * [0.])
 
 
-
 def test_peak_collection_Gaussian():
     # create test data with two peaks - both peaks will be normalized gaussians
-    NUM_SUBRUN=2
+    NUM_SUBRUN = 2
     check_peak_collection('Gaussian', NUM_SUBRUN, np.zeros(NUM_SUBRUN, dtype=get_parameter_dtype(effective=True)))
 
 
 def test_peak_collection_PseudoVoigt():
     # create test data with two peaks - both peaks will be normalized gaussians
     NUM_SUBRUN = 2
-    check_peak_collection('PseudoVoigt', NUM_SUBRUN, np.array([(3.0, 0.0,4.0,0.0,0.0,0.0,1.0),(4.0,0.0,5.0,0.0,0.0,0.0,2.0)],
-                                                     dtype=get_parameter_dtype(effective=True)))
+    check_peak_collection('PseudoVoigt', NUM_SUBRUN, np.array([(3.0, 0.0, 4.0, 0.0, 0.0, 0.0, 1.0),
+                                                               (4.0, 0.0, 5.0, 0.0, 0.0, 0.0, 2.0)],
+                                                              dtype=get_parameter_dtype(effective=True)))
+
 
 if __name__ == '__main__':
     pytest.main([__file__])
