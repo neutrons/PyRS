92ad9bc30bd66ed64d5beca9ef4cf7d7b0cf4ec6
Wenduo Zhou <zhouw@ornl.gov>
2020-01-28 23:21:22 -0500

    Refs #86.  Modified the wave length's set and get methods in HidraWorkspace.
    [Next]
    1. Test read and write methods for wave length in HidraProjectFile
    2. Apply wave length's set method alogn with calibration file
    3. Apply wave length's read/wreite methods with project file load/write.

--- pyrs/core/workspaces.py
+++ pyrs/core/workspaces.py
@@ -26,6 +26,7 @@ class HidraWorkspace(object):
         self._raw_counts = dict()  # dict [sub-run] = count vector
 
         # wave length
+        self._wave_length = None  # single wave length for all sub runs
         self._wave_length_dict = None
         self._wave_length_calibrated_dict = None
 
@@ -266,12 +267,27 @@ class HidraWorkspace(object):
 
         return self._sample_logs.subruns
 
-    def get_wavelength(self, calibrated, throw_if_not_set):
-        """ Get the wave length from the workspace
-        :param calibrated: Flag for returning calibrated wave length
-        :param throw_if_not_set: Flag to throw an exception if relative wave length (dict) is not set
-        :return:
+    def get_wavelength(self, calibrated, throw_if_not_set, sub_run=None):
+        """Get wave length
+
+        Parameters
+        ----------
+        calibrated : bool
+            whether the wave length is calibrated or raw
+        throw_if_not_set : bool
+            throw an exception if wave length is not set to workspace
+        sub_run : None or int
+            sub run number for the wave length associated with
+
+        Returns
+        -------
+        float or dict
+
         """
+        # Return the universal wave length if it is set
+        if sub_run is None and self._wave_length is not None:
+            return self._wave_length
+
         if calibrated:
             # calibrated wave length
             if self._wave_length_calibrated_dict is None:
@@ -291,6 +307,10 @@ class HidraWorkspace(object):
                     wave_length_dict = None
             else:
                 wave_length_dict = self._wave_length_dict
+
+        # Return the wave length of the sub run
+        if sub_run is not None:
+            return wave_length_dict[sub_run]
 
         return wave_length_dict
 
@@ -817,6 +837,9 @@ class HidraWorkspace(object):
         :param calibrated: Flag for calibrated wave length
         :return:
         """
+        # Set universal wave length
+        self._wave_length = wave_length
+
         # Get the sub runs
         sub_runs = self.get_sub_runs()
 
