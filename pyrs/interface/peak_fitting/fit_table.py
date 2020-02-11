af8c8dc5ed38a92666e3eabe605956f4f0754ecc
JeanBilheux <bilheuxjm@ornl.gov>
2020-02-11 17:12:19 -0500

    D-spacing display but still nan so far. this refs #339

--- pyrs/interface/peak_fitting/fit_table.py
+++ pyrs/interface/peak_fitting/fit_table.py
@@ -24,9 +24,12 @@ class FitTable:
         _peak_selected = self.parent.ui.spinBox_peak_index.value()
         _peak_collection = self.fit_result.peakcollections[_peak_selected-1]  # peak 1 is at 0 index
 
-        _value = self._get_value_to_display(_peak_collection)
+        _value = self._get_value_to_display(peak_collection=_peak_collection)
         _chisq = _peak_collection.fitting_costs
         _status = _peak_collection.get_fit_status()
+
+        _d_spacing = self._get_d_spacing_to_display(peak_selected=_peak_selected,
+                                                    peak_collection=_peak_collection)
 
         def set_item(value='', fitting_worked=True):
             _item = QTableWidgetItem(value)
@@ -51,10 +54,24 @@ class FitTable:
                 self.parent.ui.tableView_fitSummary.setItem(_row, _global_col_index, _item)
                 _global_col_index += 1
 
+            # add d-spacing
+            _item = set_item(value=str(_d_spacing[_row]), fitting_worked=_fitting_worked)
+            self.parent.ui.tableView_fitSummary.setItem(_row, _global_col_index, _item)
+            _global_col_index += 1
+
             # add status message
             _item = set_item(value=_status[_row], fitting_worked=_fitting_worked)
             self.parent.ui.tableView_fitSummary.setItem(_row, _global_col_index, _item)
             _global_col_index += 1
+
+    def _get_d_spacing_to_display(self, peak_selected=1, peak_collection=None):
+        _d_reference = np.float(str(self.parent.ui.peak_range_table.item(peak_selected-1, 3).text()))
+        peak_collection.set_d_reference(values=_d_reference)
+        values, error = peak_collection.get_dspacing_center()
+        if self.parent.ui.radioButton_fit_value.isChecked():
+            return values
+        else:
+            return error
 
     def _get_value_to_display(self, peak_collection):
         values, error = peak_collection.get_effective_params()
@@ -119,6 +136,9 @@ class FitTable:
         if self.parent.ui.radioButton_fit_value.isChecked():
             # also add chisq
             clean_column_names.append('chisq')
+
+        # add d-spacing column
+        clean_column_names.append("d spacing")
 
         # add a status column
         clean_column_names.append("Status message")
