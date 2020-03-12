437b56c59db5530c5c96effe1e7f7f21580577d2
Pete Peterson <petersonpf@ornl.gov>
2020-03-12 14:40:12 -0400

    Remove commented out line

--- pyrs/core/mantid_helper.py
+++ pyrs/core/mantid_helper.py
@@ -36,7 +36,6 @@ def generate_mantid_workspace(hidra_workspace, workspace_name, mask_id=None):
     # Mantid (2019.11) does not accept NaN
     # Convert all NaN to zero.  No good peak will have NaN or Zero
     data_y_matrix[np.where(np.isnan(data_y_matrix))] = 0.
-    # data_e_matrix = np.sqrt(data_y_matrix)
     data_e_matrix[np.where(np.isnan(data_e_matrix))] = 0.
 
     # Create Mantid workspace
