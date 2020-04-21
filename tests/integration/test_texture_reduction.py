67ee0156cc02bbf5df8b841347a21c09afeaae90
fanchercm@ornl.gov <fanchercm@ornl.gov>
2020-04-21 14:59:46 -0400

    added test for texture reduction

--- tests/integration/test_texture_reduction.py
+++ tests/integration/test_texture_reduction.py
@@ -1,12 +1,25 @@
 """
 Integration test for PyRS to calculate powder pattern from detector counts
 """
+<<<<<<< HEAD
 import numpy as np
 import h5py
 from pyrs.core.nexus_conversion import NeXusConvertingApp
 from pyrs.core.powder_pattern import ReductionApp
 import pytest
 import os
+=======
+# from pyrs.projectfile import HidraProjectFile
+import numpy as np
+import h5py
+# from pyrs.core.workspaces import HidraWorkspace
+# from pyrs.core.reduce_hb2b_pyrs import ResidualStressInstrument
+# from pyrs.core.instrument_geometry import AnglerCameraDetectorGeometry
+# from pyrs.core.reduction_manager import HB2BReductionManager
+from pyrs.core.nexus_conversion import NeXusConvertingApp
+from pyrs.core.powder_pattern import ReductionApp
+import pytest
+>>>>>>> added test for texture reduction
 
 
 def parse_gold_file(file_name):
@@ -64,7 +77,6 @@ def test_texture_reduction(nexusfile, mask_file_name, gold_file):
     -------
 
     """
-
     if not os.path.exists('/HFIR/HB2B/shared'):
         pytest.skip('Unable to access HB2B archive')
 
