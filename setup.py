ac07c5a7bb7e61da2e595e23f09f349b64486981
Mathieu Doucet <doucetm@ornl.gov>
2020-06-03 12:01:29 -0400

    Ref #438 fix conflicts

--- setup.py
+++ setup.py
@@ -66,7 +66,8 @@ if __name__ == "__main__":
     """
     scripts = ['scripts/pyrsplot',
                'scripts/reduce_manual_HB2B.py',
-               'scripts/pyrs_calibration.py']
+               'scripts/pyrs_calibration.py',
+               'scripts/create_mask.py']
     setup(
         name=NAME,
         description=find_meta("description"),
