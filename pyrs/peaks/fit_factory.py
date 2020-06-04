d2dc363d086820850fa7234ee28cb2528b816f49
Pete Peterson <petersonpf@ornl.gov>
2020-06-04 07:47:03 -0400

    Remove scipy implementation

--- pyrs/peaks/fit_factory.py
+++ pyrs/peaks/fit_factory.py
@@ -18,13 +18,9 @@ class FitEngineFactory:
 
         # this must be here for now to stop circular imports
         from .mantid_fit_peak import MantidPeakFitEngine
-        from .scipypeakfitengine import ScipyPeakFitEngine
 
         if engine_name == 'mantid':
             return MantidPeakFitEngine(hidraworkspace, peak_function_name, background_function_name,
                                        wavelength=wavelength, out_of_plane_angle=out_of_plane_angle)
-        elif engine_name == 'pyrs':
-            # this is known to be broken 2010-01-09
-            return ScipyPeakFitEngine(hidraworkspace, out_of_plane_angle)
         else:
             raise RuntimeError('Cannot create a fit engine name="{}"'.format(engine_name))
