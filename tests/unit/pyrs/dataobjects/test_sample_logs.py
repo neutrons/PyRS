7bb069a3db19733e1b66109583afe2bccafccabb
Jose Borreguero <borreguero@gmail.com>
2020-07-08 17:26:08 -0400

    Refs #521 unit test for method linear_scan of Pointlist
    Signed-off-by: Jose Borreguero <borreguero@gmail.com>

--- tests/unit/pyrs/dataobjects/test_sample_logs.py
+++ tests/unit/pyrs/dataobjects/test_sample_logs.py
@@ -91,7 +91,7 @@ class TestPointList:
         assert list(point_list.vx) == pytest.approx(sample_logs_mock['xyz'][0])
         assert list(point_list.vy) == pytest.approx(sample_logs_mock['xyz'][1])
         try:
-            _ = point_list.dummy
+            _ = point_list.dummy  # noqa F841
             assert False, 'Should not have been able to access attribute'
         except AttributeError:
             pass  # this is the correct behavior
@@ -132,6 +132,15 @@ class TestPointList:
     def test_coordinates(self, sample_logs_mock):
         point_list = PointList(sample_logs_mock['logs'])
         np.testing.assert_allclose(point_list.coordinates, np.array(sample_logs_mock['xyz']).transpose())
+
+    def test_linear_scan(self):
+        # vx and vy are only one point, within resolution
+        xyz = [[0, 0, 0.003, 0.004, 0], [0, 1, 2, 3, 4], [1, 1.001, 1.002, 1.003, 1.009]]
+        point_list = PointList(xyz)
+        assert point_list.linear_scan(resolution=DEFAULT_POINT_RESOLUTION) == pytest.approx([0, 1, 0])
+        xyz[0][0] = 0.011  # this point is beyond resolution with the rest of the other vx points
+        point_list = PointList(xyz)
+        assert point_list.linear_scan(resolution=DEFAULT_POINT_RESOLUTION) is None
 
     def test_intersection(self):
         xyz1 = [[0.0, 1.0, 2.0, 3.0], [0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0]]
