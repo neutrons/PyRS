e162d417b92266ee8ced974fec48243e8d96a9ba
fanchercm <32335915+fanchercm@users.noreply.github.com>
2023-02-22 11:58:24 -0500

    Revert "Add d0 grid validation (#835)" (#836)
    This reverts commit 28a17f44eadcddf9f30b461d7c63c30018102fb5.

--- pyrs/interface/strainstressviewer/model.py
+++ pyrs/interface/strainstressviewer/model.py
@@ -1,7 +1,5 @@
 import json
 import traceback
-import numpy as np
-import pandas as pd
 from pyrs.dataobjects.fields import StressField, StrainField, ScalarFieldSample
 from pyrs.core.stress_facade import StressFacade
 from pyrs.core.summary_generator_stress import SummaryGeneratorStress
@@ -199,32 +197,6 @@ class Model(QObject):
         self._youngs_modulus = youngModulus
         self._poisson_ratio = poissonsRatio
         self._d0 = d0
-
-    def validate_d0_grid_data(self, x_grid, y_grid, z_grid, d0_grid, d0e_grid, default_d0, default_d0e):
-        # get stress facade values to validate d0
-        n_decimals = 3
-        stress = self.stress
-
-        # convert to pandas df for joins
-        stress_stacked = np.column_stack((stress.x.round(n_decimals), stress.y.round(n_decimals), stress.z.round(n_decimals)))
-        grid_stacked = np.column_stack((x_grid.round(n_decimals), y_grid.round(n_decimals), z_grid.round(n_decimals), d0_grid, d0e_grid))
-        stress_df = pd.DataFrame(stress_stacked, columns = ['x', 'y', 'z'])
-        grid_df = pd.DataFrame(grid_stacked, columns = ['x', 'y', 'z', 'd0', 'd0e'])
-
-        # left join to show differences between dfs
-        full_df = stress_df.merge(grid_df, on=["x", "y", "z"], how="left")
-
-        # logic to find missing data after join
-        if not (full_df['d0'].isnull().values.any() or full_df['d0e'].isnull().values.any()):
-            full_df.loc[np.isnan(full_df["d0"]), "d0"] = default_d0
-            full_df.loc[np.isnan(full_df["d0e"]), "d0e"] = default_d0e
-            return 1, full_df['x'].to_numpy(), full_df['y'].to_numpy(), full_df['z'].to_numpy(), full_df['d0'].to_numpy(), full_df['d0e'].to_numpy()
-        elif (full_df['d0'].isnull().values.all() or full_df['d0e'].isnull().values.all()):
-            return -1, None, None, None, None, None
-        else:
-            full_df.loc[np.isnan(full_df["d0"]), "d0"] = default_d0
-            full_df.loc[np.isnan(full_df["d0e"]), "d0e"] = default_d0e
-            return 0, full_df['x'].to_numpy(), full_df['y'].to_numpy(), full_df['z'].to_numpy(), full_df['d0'].to_numpy(), full_df['d0e'].to_numpy()
 
     def write_stress_to_csv(self, filename, detailed):
         try:
