503152fbe496f2d1f04dd33bd888ef2841cd1463
Chris Fancher <fanchercm@ornl.gov>
2025-02-04 14:42:01 -0500

    fix assert length for added lines in summary file

--- tests/ui/test_peak_fitting.py
+++ tests/ui/test_peak_fitting.py
@@ -88,7 +88,7 @@ def test_peak_fitting(tmpdir, fit_peaks_window):
     assert os.path.isfile("HB2B_1423.csv")
     file_contents = open("HB2B_1423.csv").readlines()
     # check number of lines
-    assert len(file_contents) == 127
+    assert len(file_contents) == 129
     # check number of values in line
     assert len(np.fromstring(file_contents[-1], dtype=np.float64, sep=',')) == 35
 
@@ -132,7 +132,7 @@ def test_peak_fitting(tmpdir, fit_peaks_window):
     assert os.path.isfile("HB2B_1423.csv")
     file_contents = open("HB2B_1423.csv").readlines()
     # check number of lines
-    assert len(file_contents) == 127
+    assert len(file_contents) == 129
     # check number of values in line
     assert len(np.fromstring(file_contents[-1], dtype=np.float64, sep=',')) == 35
 
