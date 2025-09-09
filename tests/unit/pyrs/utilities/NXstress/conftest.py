2bee7ce2d047f30a27d90a0b789d4d72f57b06e4
Kort Travis <traviska@ornl.gov>
2025-09-09 10:53:00 -0400

    Add '[tool.pytest.ini_options]' to 'pyproject.toml' so that we can have a 'tests/util' section.

--- tests/unit/pyrs/utilities/NXstress/conftest.py
+++ tests/unit/pyrs/utilities/NXstress/conftest.py
@@ -1,3 +1,10 @@
 
+import sys
+
+print('******************************')
+print(f'******** {sys.path} *********')
+print('******************************')
+
+
 from util.peak_collection_helpers import createPeakCollection
 from util.workspace_helpers import loadHidraWorkspace
