7bbf98c8753b18bc8244d89f4b812fa9a5aa8447
Fancher <hcf@mac130340.ornl.gov>
2025-09-23 09:59:47 -0400

    added pyrsplot to pixi setup

--- scripts/pyrsplot.py
+++ scripts/pyrsplot.py
@@ -6,21 +6,22 @@ from qtpy.QtWidgets import QDialog, QApplication
 import pyrs.interface.pyrs_main
 
 
-def main(argv):
+def main(argv=None):
     """
     """
+    if argv is None:
+        argv = sys.argv
     if QApplication.instance():
         _app = QApplication.instance()
     else:
-        _app = QApplication(sys.argv)
+        _app = QApplication(argv)
+    main_window = pyrs.interface.pyrs_main.PyRSLauncher()  # .FourCircleMainWindow()
+    main_window.show()
+
+    _app.exec_()
     return _app
 
 
 if __name__ == '__main__':
     # Main application
-    app = main(sys.argv)
-
-    main_window = pyrs.interface.pyrs_main.PyRSLauncher()  # .FourCircleMainWindow()
-    main_window.show()
-
-    app.exec_()
+    app = main()
